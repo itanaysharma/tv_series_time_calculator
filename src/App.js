e677060b2e705f77275a33fc0b6af181f476739a
itanaysharma <thisistanaysharma@gmail.com>
2023-04-20 23:46:24 +0530

    Created an API call to get all the episodes will handle tomorrow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import "./App.css";
 import React from "react";
-
+const API_ENDPOINT = "https://api.tvmaze.com/singlesearch/shows?q=";
 function App() {
-  const [timer, setTimer] = React.useState([0, 0, 0]);
+  const [totalTime, setTotalTimer] = React.useState([0, 0, 0]);
+  const [seriesName, setSeriesName] = React.useState("");
+  const [apiCall, setApiCall] = React.useState("");
+
+  const handleName = (event) => {
+    // console.log(event.target.value);
+    setSeriesName(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    const name = seriesName.replaceAll(" ", "+");
+    setApiCall(name);
+    console.log(apiCall);
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <form className="mainform">
-        <h1 className="timer">
-          {timer[0].toString() +
-            " : " +
-            timer[1].toString() +
-            " : " +
-            timer[2].toString()}
-        </h1>
-        <input
-          className="movie_text"
-          type="text"
-          placeholder="TV Series Name"
-        ></input>
-      </form>
+    <div className="mainform">
+      <h1 className="timer">
+        {totalTime[0].toString() +
+          " : " +
+          totalTime[1].toString() +
+          " : " +
+          totalTime[2].toString()}
+      </h1>
+      <div>
+        <TvSeries saveName={handleName} final_submit={handleSubmit}></TvSeries>
+        {apiCall.length !== 0 && <API series={apiCall}></API>}
+      </div>
     </div>
   );
 }
 
+const TvSeries = ({ saveName, final_submit }) => (
+  <form onSubmit={final_submit}>
+    <input
+      className="movie_text"
+      type="text"
+      placeholder="TV Series"
+      onChange={saveName}
+    ></input>
+    <button type="submit" className="button">
+      Cl
+    </button>
+  </form>
+);
+
+const API = (series) => {
+  const runTime = 0;
+  console.log("hello");
+  fetch(`${API_ENDPOINT}${series}&embed=episodes`)
+    .then((response) => response.json())
+    .then((result) => console.log(result._embedded.episodes));
+};
+// const URL = ()=>(
+
+// )
+
 export default App;
+
+//
